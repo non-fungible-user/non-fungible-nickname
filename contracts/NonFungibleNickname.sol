5afe90fc43a4f823fb3c1588c29a96b5562013a3
Non Fungible User <oleh.pashchenko+1@gmail.com>
2022-10-12 15:01:51 +0200

    Added On-Chain metadata

--- contracts/NonFungibleNickname.sol
+++ contracts/NonFungibleNickname.sol
@@ -5,7 +5,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
 contract NonFungibleNickname is ERC721Burnable, Pausable, Ownable {
+    using Strings for uint256;
+
     uint256 private idDigits = 18;
     uint256 private idModulus = 10**idDigits;
     uint256 private mintPrice = 0.02 ether;
@@ -34,6 +39,64 @@ contract NonFungibleNickname is ERC721Burnable, Pausable, Ownable {
     {
         uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
         return rand % idModulus;
+    }
+
+    function generateCard(uint256 tokenId) public view returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="250" height="250" viewBox="0 0 250 250" version="1.1" fill="none">',
+            "<title>Non Fungible Nickname #",
+            nicknames[tokenId],
+            "</title>",
+            '<g id="unstoppabledomains" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">',
+            '<path fill="#000" d="M0 0 H250 V250 H0 V0 z"/>',
+            '<g id="Group-6" transform="translate(70.000000, 105.000000)">',
+            '<text text-anchor="middle" id="domain" font-family="system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, Oxygen, Cantarell, sans-serif" font-size="20" font-weight="bold" fill="#FFFFFF">',
+            '<tspan x="22.5%" y="26" class= "nickname">',
+            nicknames[tokenId],
+            "</tspan>",
+            "</text></g></g></svg>"
+        );
+
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+            '"name": "Non Fungible Nickname #',
+            nicknames[tokenId],
+            '",',
+            '"description": "Your non fungible nickname",',
+            '"image": "',
+            generateCard(tokenId),
+            '",',
+            '"attributes": [{',
+            '"trait_type": "Nickname",',
+            '"value":"',
+            nicknames[tokenId],
+            '"',
+            "}]",
+            "}"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(dataURI)
+                )
+            );
     }
 
     function generateRandomId(string memory _name)
